aabefd4b169150b528c08034dc229ef579742a1e
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-19 04:06:34 -0500

    Enhance navigation bar appearance in AppDelegate
    Added custom navigation bar appearance with specific colors and fonts.

--- MiApp/AppDelegate.m
+++ MiApp/AppDelegate.m
@@ -4,10 +4,23 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    UIColor *acento = [UIColor colorWithRed:0.2 green:1.0 blue:0.5 alpha:1.0];
+
+    UINavigationBarAppearance *ap = [[UINavigationBarAppearance alloc] init];
+    [ap configureWithOpaqueBackground];
+    ap.backgroundColor = [UIColor blackColor];
+    ap.shadowColor = [UIColor colorWithWhite:0.25 alpha:1.0];
+    ap.titleTextAttributes = @{
+        NSForegroundColorAttributeName: acento,
+        NSFontAttributeName: [UIFont fontWithName:@"Menlo-Bold" size:17]
+    };
+    [[UINavigationBar appearance] setStandardAppearance:ap];
+    [[UINavigationBar appearance] setScrollEdgeAppearance:ap];
+    [[UINavigationBar appearance] setTintColor:acento];
+
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     ViewController *vc = [[ViewController alloc] init];
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
-    nav.navigationBar.barStyle = UIBarStyleBlack;
     self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
     return YES;
