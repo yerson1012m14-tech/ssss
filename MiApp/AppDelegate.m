a677e6d90b20e5c2a4048a558ab255ade7081f44
yerson1012m14-tech <yerson1012m14@gmail.com>
2026-08-19 03:46:42 -0500

    Add UINavigationController to AppDelegate

--- MiApp/AppDelegate.m
+++ MiApp/AppDelegate.m
@@ -2,11 +2,15 @@
 #import "ViewController.h"
 
 @implementation AppDelegate
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     ViewController *vc = [[ViewController alloc] init];
-    self.window.rootViewController = vc;
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+    nav.navigationBar.barStyle = UIBarStyleBlack;
+    self.window.rootViewController = nav;
     [self.window makeKeyAndVisible];
     return YES;
 }
+
 @end
